d18f589716f200fa75612b2a33ce57428006f763
Zamshed Forman <64319245+Zamshed87@users.noreply.github.com>
2022-03-14 23:50:08 +0600

    Update links.php

--- iwt_project/link/links.php
+++ iwt_project/link/links.php
@@ -3,12 +3,21 @@
 
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@200&display=swap" rel="stylesheet">
 
 
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+<link href="https://fonts.googleapis.com/css2?family=Muli&display=swap" rel="stylesheet">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counter"></script>
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/ jquery/3.4.1/jquery.min.js"></script> <script src="https://cdnjs.cloudflare.com/ajax/libs/ waypoints/4.0.1/jquery.waypoints.min.js" integrity=" sha256-jDnOKIOq2KNsQZTcBTEnsp76FnfmEttF6AV2DF2FFNE=" crossorigin=" anonymous"></script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/ jquery.counterup.min.js" integrity=" sha256-JtQPj/3xub8oapVMalijPNOMODHOAtgh/gwFYUN5rik=" crossorigin=" anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/ popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/ bootstrap.min.js"></script>
